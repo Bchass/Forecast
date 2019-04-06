da6a07e3a74c97f2bdf38d5524a8d1f22cc4717f
Brandon Chasser <brandonchasser22@gmail.com>
2019-04-05 20:19:40 -0400

    Formatting update

--- app.js
+++ app.js
@@ -1,107 +1,107 @@
 window.addEventListener('load', () => {
-    let long;
-    let lat;
+  let long;
+  let lat;
 
-    // Current
-    const temperatureDescription = document.querySelector(".temperature-description");
-    const temperatureDegree = document.querySelector(".temperature-degree");
-    const temperatureSection = document.querySelector(".temperature");
-    const temperatureSpan = document.querySelector(".temperature span");
-    const locationTimeZone = document.querySelector(".location-timezone");
+  // Current
+  const temperatureDescription = document.querySelector(".temperature-description");
+  const temperatureDegree = document.querySelector(".temperature-degree");
+  const temperatureSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".temperature span");
+  const locationTimeZone = document.querySelector(".location-timezone");
 
-    // Hourly
-    const hourlyTemperature = document.querySelector(".hourly-temp");
-    const hourlyDescription = document.querySelector(".hourly-description");
+  // Hourly
+  const hourlyTemperature = document.querySelector(".hourly-temp");
+  const hourlyDescription = document.querySelector(".hourly-description");
 
-    // Daily
-    const dailyTemperature = document.querySelector(".daily-temp");
-    const dailyDescription = document.querySelector(".daily-description");
+  // Daily
+  const dailyTemperature = document.querySelector(".daily-temp");
+  const dailyDescription = document.querySelector(".daily-description");
 
-    // Gets current location
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+  // Gets current location
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-            // proxy to overide localhost
-            const proxy = "https://cors-anywhere.herokuapp.com/"
-            // api for data
-            const api = `${proxy}https://api.darksky.net/forecast/a7bc488da17321434c36f41fe7c48b07/${lat},${long}`;
+      // proxy to overide localhost
+      const proxy = "https://cors-anywhere.herokuapp.com/"
+      // api for data
+      const api = `${proxy}https://api.darksky.net/forecast/a7bc488da17321434c36f41fe7c48b07/${lat},${long}`;
 
-            // Current data
-            fetch(api)
+      // Current data
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const {
+            temperature,
+            summary,
+            icon
+          } = data.currently;
+
+          // Current
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimeZone.textContent = data.timezone;
+
+
+          // Hourly data
+          fetch(api)
+            .then(response => {
+              return response.json();
+            })
+            .then(data => {
+              const {
+                summary
+              } = data.hourly;
+
+              // Hourly
+              hourlyDescription.textContent = summary;
+
+              // Daily data
+              fetch(api)
                 .then(response => {
-                    return response.json();
+                  return response.json();
                 })
                 .then(data => {
-                    const {
-                        temperature,
-                        summary,
-                        icon
-                    } = data.currently;
+                  const {
+                    summary
+                  } = data.daily;
 
-                    // Current
-                    temperatureDegree.textContent = temperature;
-                    temperatureDescription.textContent = summary;
-                    locationTimeZone.textContent = data.timezone;
+                  // Daily
+                  dailyDescription.textContent = summary;
 
+                  // Formula for celisus
+                  let celisus = (temperature - 32) * (5 / 9);
 
-                    // Hourly data
-                    fetch(api)
-                        .then(response => {
-                            return response.json();
-                        })
-                        .then(data => {
-                            const {
-                                summary
-                            } = data.hourly;
+                  // Set icon
+                  setIcons(icon, document.querySelector(".icon"));
 
-                            // Hourly
-                            hourlyDescription.textContent = summary;
-
-                            // Daily data
-                            fetch(api)
-                                .then(response => {
-                                    return response.json();
-                                })
-                                .then(data => {
-                                    const {
-                                        summary
-                                    } = data.daily;
-
-                                    // Daily
-                                    dailyDescription.textContent = summary;
-
-                                    // Formula for celisus
-                                    let celisus = (temperature - 32) * (5 / 9);
-
-                                    // Set icon
-                                    setIcons(icon, document.querySelector(".icon"));
-
-                                    // Change temp between celisus/fahrenheit
-                                    temperatureSection.addEventListener('click', () => {
-                                        if (temperatureSpan.textContent === "F") {
-                                            temperatureSpan.textContent = "C";
-                                            temperatureDegree.textContent = Math.floor(celisus);
-                                        } else {
-                                            temperatureSpan.textContent = "F";
-                                            // Gets temp back from api
-                                            temperatureDegree.textContent = Math.round(temperature);
-                                        }
-                                    })
-                                    console.log(data);
-                                });
-                        });
-                })
-            // Format for skyicons
-            function setIcons(icon, iconID) {
-                const skycons = new Skycons({
-                    color: "white"
+                  // Change temp between celisus/fahrenheit
+                  temperatureSection.addEventListener('click', () => {
+                    if (temperatureSpan.textContent === "F") {
+                      temperatureSpan.textContent = "C";
+                      temperatureDegree.textContent = Math.floor(celisus);
+                    } else {
+                      temperatureSpan.textContent = "F";
+                      // Gets temp back from api
+                      temperatureDegree.textContent = Math.round(temperature);
+                    }
+                  })
+                  console.log(data);
                 });
-                const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-                skycons.play();
-                return skycons.set(iconID, Skycons[currentIcon]);
-            }
+            });
+        })
+      // Format for skyicons
+      function setIcons(icon, iconID) {
+        const skycons = new Skycons({
+          color: "white"
         });
-    };
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+      }
+    });
+  };
 });
